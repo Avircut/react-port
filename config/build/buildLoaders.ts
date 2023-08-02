9c49c5e1227377c6411fc731e3a8eb0168551ae0
Avircut <lomoff.slaw@yandex.ru>
2023-08-02 11:03:17 +0700

    initial

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,30 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const tsLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: "/node_modules",
+  };
+  const sassLoader = {
+    test: /\.s[ac]ss$/,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
+  return [tsLoader, sassLoader];
+}
