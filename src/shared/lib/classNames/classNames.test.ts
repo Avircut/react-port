fa2a4a0e4c7902d244ae823d2d6207b219e72243
Avircut <lomoff.slaw@yandex.ru>
2023-08-04 18:09:01 +0700

    jest tests added

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,26 @@
+import { classNames } from './classNames';
+
+describe('Classnames', () => {
+  test('with first param', () => {
+    expect(classNames('class')).toBe('class');
+  });
+  test('with three params', () => {
+    const expected = 'class class1 class3';
+    expect(classNames('class', {}, ['class1', 'class3'])).toBe(expected);
+  });
+  test('with mods', () => {
+    expect(classNames('class', { selected: true, highlight: true }, ['class1', 'class3'])).toBe(
+      'class class1 class3 selected highlight',
+    );
+  });
+  test('with mods false', () => {
+    expect(classNames('class', { selected: false, highlight: false }, ['class1', 'class3'])).toBe(
+      'class class1 class3',
+    );
+  });
+  test('with mods undefined', () => {
+    expect(classNames('class', { selected: false, highlight: undefined }, ['class1', 'class3'])).toBe(
+      'class class1 class3',
+    );
+  });
+});
