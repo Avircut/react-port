b293060e7fd0dfcf4132b93cf89ee11186b6636c
Avircut <lomoff.slaw@yandex.ru>
2023-08-05 18:32:47 +0700

    Fix sidebar test

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import { ReactNode } from 'react';
-import i18n from '../../config/i18n/i18nForTests';
+import i18n from '../../../config/i18n/i18nForTests';
 
 export function renderWithTranslation(component:ReactNode) {
   return render(
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,14 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
-import { renderWithTranslation } from '../../../../shared/lib/tests/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
   test('with only first param', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
   test('with only first param', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const btn = screen.getByTestId('toggle-btn');
     fireEvent.click(btn);
     expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
